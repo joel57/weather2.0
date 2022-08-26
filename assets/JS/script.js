fad85239ec37ed7172bbdb3e6265e74edf4ef03f
joel57 <67278919+joel57@users.noreply.github.com>
2022-08-26 12:14:02 -0700

    testing the api

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,13 +1,28 @@
-var button = document.querySelector('.button')
-var cityInput = document.querySelector('.cityInput')
+// var button = document.querySelector('.button')
+// var cityInput = document.querySelector('.cityInput')
 
 
 
-button.addEventListener('click',function(){
-    fetch("api.openweathermap.org/data/2.5/forecast?q'+cityInput+'&appid=2eca45ffa243e0add68c63c8e50768b3")
-    .then(response => response.json())
-    .then(data => console.log(data))
+// button.addEventListener('click',function(){
+//     fetch("api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=2eca45ffa243e0add68c63c8e50768b3")
+//     .then(response => response.json())
+//     .then(data => console.log(data))
     
-    .catch(err => alert("Please add city name"))
+//     .catch(err => alert("Please add city name"))
 
-})
+// })
+//Yess Tutor 8/26/22
+function search(){
+
+    var city = $("#cityInput").val();
+    // comprovamos
+    console.log(city);
+
+    // fetch("https://api.openweathermap.org/data/2.5/weather?q="+city+"&appid=2eca45ffa243e0add68c63c8e50768b3")
+    // .then(response => response.json())
+    // .then(data => console.log(data))
+
+}
+
+
+
